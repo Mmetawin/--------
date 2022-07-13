4a5a8069921b0342fafff05beffe9f914d4d4151
Mmetawin <3159141037@qq.com>
2022-07-13 15:48:44 +0800

    [feature]nothing

--- src/test.java
+++ src/test.java
@@ -1,5 +1,7 @@
 public class test {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Hello World!");
+        int x=100;
+        System.out.println(x);
     }
 }
